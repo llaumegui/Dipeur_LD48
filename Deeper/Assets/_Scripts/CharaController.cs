d0644565305f2083c8bdd74830ffbf173b3d4ed2
llaumegui <guillaumelef59@gmail.com>
2021-04-24 13:51:04 +0200

    BaseController + passage version 4.12

---
+++ Deeper/Assets/_Scripts/CharaController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaController : MonoBehaviour
+{
+    public Vector2 MaxDirs;
+
+    [Header("Characters")]
+    public GameObject Knight;
+    public GameObject Wizard;
+    public Vector2 PlayersPos;
+    float _wizardY = 5;
+
+    Transform _knight;
+    Transform _wizard;
+
+    [Header("Movement")]
+    [Range(0, 1)]public float Speed;
+    [Range(0,1)]public float InertiaIntensity;
+    float _xValue;
+    float _yValue;
+
+    [Header("Keycodes")]
+    public KeyCode DropBomb;
+
+    private void Awake()
+    {
+        _knight = Knight.GetComponent<Transform>();
+        _wizard = Wizard.GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        ApplyMovement();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Inputs();
+    }
+
+    void Inputs()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Speed;
+        float y = Input.GetAxisRaw("Vertical") * Speed;
+
+        _xValue = Mathf.Lerp(_xValue, x, 1-InertiaIntensity);
+        _yValue = Mathf.Lerp(_yValue, y, 1 - InertiaIntensity);
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        float x = PlayersPos.x + _xValue;
+        float y = PlayersPos.y + _yValue;
+
+        if(x>MaxDirs.x || x<-MaxDirs.x)
+        {
+            if (x > MaxDirs.x)
+                x = MaxDirs.x;
+            else
+                x = -MaxDirs.x;
+        }
+
+        if (y > MaxDirs.y || y < -MaxDirs.y)
+        {
+            if (y > MaxDirs.y)
+                y = MaxDirs.y;
+            else
+                y = -MaxDirs.y;
+        }
+
+        PlayersPos.x = x;
+        PlayersPos.y = y;
+
+        ApplyMovement();
+    }
+
+    void ApplyMovement()
+    {
+        _knight.position = PlayersPos;
+        _wizard.position = new Vector2(-PlayersPos.x, _wizardY);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-MaxDirs.x, _wizardY, 0), new Vector3(MaxDirs.x, _wizardY, 0));
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(new Vector3(0,-MaxDirs.y, 0), new Vector3(0,MaxDirs.y, 0));
+    }
+}
---
+++ Deeper/Assets/_Scripts/GameMaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    #region Singleton
+    static GameMaster _i;
+    public static GameMaster I { get
+        {
+            return _i;
+        }
+    }
+    #endregion
+
+
+
+    private void Awake()
+    {
+        if (_i != null && _i != this)
+            Destroy(gameObject);
+
+        _i = this;
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
