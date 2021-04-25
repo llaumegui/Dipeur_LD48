400125fce5dd49735cf0be51eba548bd9a82a625
Antoine Nisoli <nisoliantoine@outlook.com>
2021-04-25 04:37:38 +0200

    chain animation

---
+++ Deeper/Assets/_Scripts/AntoineN/FixHookPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixHookPosition : MonoBehaviour
+{
+    [SerializeField] Transform anchor;
+    Vector2 startPos;
+    HingeJoint2D joint2d;
+
+    private void Awake()
+    {
+        joint2d = GetComponent<HingeJoint2D>();
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+       joint2d.anchor = new Vector2(anchor.position.x, startPos.y);
+    }
+}
---
+++ Deeper/Assets/_Scripts/AntoineN/LimitVelocity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitVelocity : MonoBehaviour
+{
+    [SerializeField] Vector2 maxVelocity;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.rotation = Mathf.Clamp(rb.rotation, maxVelocity.x, maxVelocity.y);
+        Quaternion currentTor = transform.rotation;
+        Vector3 vector = currentTor.eulerAngles;
+        vector.z = Mathf.Clamp(vector.z, maxVelocity.x, maxVelocity.y);
+        currentTor.eulerAngles = vector;
+        //transform.rotation = currentTor;
+    }
+}
