e5b0f2c3c3e4ee1da92b3134fc6fbc65c6c5d63a
llaumegui <guillaumelef59@gmail.com>
2021-04-26 23:13:36 +0200

    MainMenu

---
+++ Deeper/Assets/_Scripts/UI/AnimationTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimationTitle : MonoBehaviour
+{
+    public GameObject MenuToSpawn;
+    public bool ChangeScale;
+
+    private void Start()
+    {
+        if(ChangeScale)
+        {
+            transform.localScale = Vector3.one * 2f;
+            transform.DOScale(1, .5f);
+        }
+
+        StartCoroutine(SpawnMenu());
+    }
+
+    IEnumerator SpawnMenu()
+    {
+        yield return new WaitForSeconds(1.5f);
+        MenuToSpawn.SetActive(true);
+    }
+}
---
+++ Deeper/Assets/_Scripts/UI/SpriteSetter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteSetter : MonoBehaviour
+{
+    Image _img;
+    public Sprite DefaultSprite;
+    public List<Sprite> Sprites;
+
+    int _chancePercent = 10;
+
+    private void OnEnable()
+    {
+        _img = GetComponent<Image>();
+        if (Sprites.Count > 0)
+            RandomSprite();
+    }
+
+    void RandomSprite()
+    {
+        int r = Random.Range(1, 101);
+        Debug.Log(r);
+        if (r <= _chancePercent)
+        {
+            int randomSprite = Random.Range(0, Sprites.Count);
+            _img.sprite = Sprites[randomSprite];
+        }
+        else
+            _img.sprite = DefaultSprite;
+    }
+}
